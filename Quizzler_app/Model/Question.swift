bd723123af6a2c75f2e7bdd4f769089a14044b99
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-03-18 00:27:15 +0300

    model created

---
+++ Quizzler_app/Model/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quizzler_app
+//
+//  Created by AYTAÇ BULANIK on 18.03.2022.
+//
+
+import Foundation
+
+struct Question {
+    
+    let q : String
+    let a : [String]
+    let correctAnswer : String
+}
